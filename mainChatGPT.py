42d67399da75f69880680a3a5b2b80065d855f09
Alibek Kaliyev <alk224@lehigh.edu>
2022-12-27 23:09:06 +0600

    separated chatGPT and GPT-3 versions

--- mainChatGPT.py
+++ mainChatGPT.py
@@ -2,14 +2,17 @@ import streamlit as st
 from pyChatGPT import ChatGPT
 from youtube_transcript_api import YouTubeTranscriptApi
 import os
-import openai
-from dotenv import load_dotenv
-
-load_dotenv()
 
 st.header("Get podcast notes using AI")
 
 st.write('GitHub repo for this project: https://github.com/abekek/podcast_notes_chatgpt')
+
+# uncomment if want to use session token
+# session_token = st.text_input('Session token from ChatGPT')
+
+st.write('Enter your ChatGPT credentials below. If you don\'t have an account, you can sign up for free at https://openai.com/blog/chatgpt/')
+email = st.text_input('Email')
+password = st.text_input('Password', type="password")
 
 st.markdown("""---""")
 
@@ -17,10 +20,14 @@ yt_id = st.text_input('YouTube video ID')
 
 bpm = st.slider('Choose # of bullet points/minute', 1, 5)
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
 if st.button('Get notes'):
     result = YouTubeTranscriptApi.get_transcript(yt_id)
+
+    api = ChatGPT(auth_type='openai', email=email, password=password,
+                verbose=True)  # auth with email and password
+
+    # uncomment if want to use session token
+    # api = ChatGPT(session_token)
 
     curr_time = 0
     interval = 1
@@ -41,18 +48,11 @@ if st.button('Get notes'):
 
     for i in range(len(text)):
         print('test')
-        # res = api.send_message(prompt + text[i])['message'] + "\n"
-        res = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=prompt + text[i] + '\nSummary:\n',
-            temperature=0.6,
-            max_tokens=576,
-            top_p=1,
-            frequency_penalty=1,
-            presence_penalty=1
-        )
-        res_text = res['choices'][0]['text']
+        res = api.send_message(prompt + text[i])['message'] + "\n"
+        res_text = res
         st.write('Block ' + str(i+1) + '/' + str(len(text)) + ':')
         st.write(res_text)
         notes += res_text
-        
+
+    api.reset_conversation()  # reset the conversation
+    api.refresh_chat_page()  # refresh the chat page
